80d6ceecd9fec3ee1f62f5afe9cdbfb5d90af8f2
Connor Lane <cclane2022@gmail.com>
2022-02-11 14:25:38 -0600

    Exercise Completed

---
+++ script.js
@@ -0,0 +1,41 @@
+// 1
+console.log(7); // Could also be console.log("7");
+
+// 2 - Numbers, Strings, Booleans, Null, Undefined
+
+// 3 - + (Addition), - (Subtraction), * (Multiplication), / (Division), ** (Exponentiation/Exponents), % (Modulus/Remainder)
+
+// 4
+console.log(5 % 4);
+console.log(5 % -4);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Connor".length + "Lane".length);
+
+// 8
+console.log("Endgame"[6]);
+
+// BONUS
+// 1
+console.log("\\");
+
+// 2
+console.log(5 ** 4 - 9);
+
+// 3
+console.log("I".length + "Am".length % "Iron".length - "Man".length);
+
+// 4
+console.log("CAP"[1] + "VISION"[0] + "OKOYE"[4] + "NICK FURY"[0] + "GROOT"[0] + "DOCTOR STRANGE"[13] + "THOR"[3] + "STAR-LORD"[0]);
+
+// 5
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
